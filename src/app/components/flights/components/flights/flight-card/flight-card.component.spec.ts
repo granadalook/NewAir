770574f2f73a22dd55d896257eef9d18896be80f
granadalook <tiven17.jsga@gmail.com>
2023-05-07 13:35:45 -0500

    ADD TEST : se agregan test para flight-card.component.spec

--- src/app/components/flights/components/flights/flight-card/flight-card.component.spec.ts
+++ src/app/components/flights/components/flights/flight-card/flight-card.component.spec.ts
@@ -4,11 +4,13 @@ import { FlightCardComponent } from './flight-card.component';
 import { CurrencyService } from 'src/app/core/services/currency/currency.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IJourney } from 'src/app/models/journey.model';
+import { of } from 'rxjs';
 
-describe('FlightCardComponent', () => {
+fdescribe('FlightCardComponent', () => {
   let component: FlightCardComponent;
   let fixture: ComponentFixture<FlightCardComponent>;
-
+  let currencyService: CurrencyService;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [FlightCardComponent],
@@ -16,6 +18,7 @@ describe('FlightCardComponent', () => {
       providers: [CurrencyService],
     }).compileComponents();
 
+    currencyService = TestBed.inject(CurrencyService);
     fixture = TestBed.createComponent(FlightCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -23,5 +26,40 @@ describe('FlightCardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set formatprice value whit the input journeyprice when running NgOnInit', () => {
+    const journey: IJourney = {
+      flights: [],
+      origin: 'testOrigin',
+      destination: 'testDestination',
+      price: 100,
+    };
+
+    component.journey = journey;
+    component.ngOnInit();
+    expect(component.formatePrice).toEqual(journey.price);
+    expect(component.currencies.length).toBeGreaterThan(0);
+  });
+
+  it('should convert journey price to specified currency', () => {
+    const journey: IJourney = {
+      flights: [],
+      origin: 'testOrigin',
+      destination: 'testDestination',
+      price: 100,
+    };
+    const to = 'USD';
+    const expected = 80;
+    spyOn(currencyService, 'convert').and.returnValue(of({ result: expected }));
+
+    component.convert(journey, to);
+
+    expect(currencyService.convert).toHaveBeenCalledWith(
+      to,
+      component.from,
+      journey.price
+    );
+    expect(component.formatePrice).toEqual(expected);
   });
 });
