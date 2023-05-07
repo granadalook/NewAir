795ef7f1fc1e4c413ec7c199c1cf702660730014
granadalook <tiven17.jsga@gmail.com>
2023-05-07 04:52:56 -0500

    ADD:Se crea servico de conversion de moneda  para mas de 10 monedas importantes a  nivel mundial

--- src/app/components/flights/components/flights/flights.component.ts
+++ src/app/components/flights/components/flights/flights.component.ts
@@ -1,4 +1,10 @@
 import { Component, Input } from '@angular/core';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { ConversionService } from 'src/app/core/services/conversion/conversion-service.service';
 import { IJourney } from 'src/app/models/journey.model';
 
 @Component({
@@ -7,6 +13,23 @@ import { IJourney } from 'src/app/models/journey.model';
   styleUrls: ['./flights.component.scss'],
 })
 export class FlightsComponent {
-  @Input() journeys?: Array<IJourney>;
+  from: string = 'USD';
 
+  @Input() journeys?: Array<IJourney>;
+  constructor(
+    private conversionService: ConversionService,
+    private formBuilder: UntypedFormBuilder
+  ) {}
+
+  currencyForm: UntypedFormGroup = this.formBuilder.group({
+    currency: ['', [Validators.required, Validators.minLength(3)]],
+  });
+  convert(journey: IJourney, to: string) {
+    this.conversionService
+      .convertCurrency(to, this.from, journey.price)
+      .subscribe((data) => {
+        this.from = to;
+        journey.price = data.result;
+      });
+  }
 }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,3 +1,5 @@
 export const environment = {
   URL_BASE: 'https://recruiting-api.newshore.es/api/flights/2',
+  API_KEY: 'MMMsICDQAedEwcBXQhdr9M3sbhhgKUJj',
+  API_COMVERT_URL: 'https://api.apilayer.com/currency_data/convert?',
 };
